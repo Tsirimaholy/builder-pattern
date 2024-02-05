971502f30c7fac77dea0b97a8cb765e9186fd192
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-02-05 15:13:45 +0300

    feat: adding more attribute to gps

--- src/main/java/org/example/components/Engine.java
+++ src/main/java/org/example/components/Engine.java
@@ -1,4 +1,61 @@
 package org.example.components;
 
 public class Engine {
+    // gasoline, diesel, hybrid
+    private String fuelType;
+    private Integer topSpeed;
+    // L/km
+    private Integer fuelEfficiency;
+    private Integer manufactureYear;
+    private Double weight;
+    public Engine(){
+
+    }
+    public Engine(String fuelType, Integer topSpeed, Integer fuelEfficiency, Integer manufactureYear, Double weight) {
+        this.fuelType = fuelType;
+        this.topSpeed = topSpeed;
+        this.fuelEfficiency = fuelEfficiency;
+        this.manufactureYear = manufactureYear;
+        this.weight = weight;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Integer getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(Integer topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public Integer getFuelEfficiency() {
+        return fuelEfficiency;
+    }
+
+    public void setFuelEfficiency(Integer fuelEfficiency) {
+        this.fuelEfficiency = fuelEfficiency;
+    }
+
+    public Integer getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(Integer manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
 }
--- src/main/java/org/example/components/GPS.java
+++ src/main/java/org/example/components/GPS.java
@@ -1,4 +1,26 @@
 package org.example.components;
 
 public class GPS {
+    private String destination;
+    private String currentLocation;
+
+    public GPS(String destination) {
+        this.destination = destination;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
 }
