2ddc1857fb8bdc57151318881dab7da878c6e47d
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-02-05 15:30:20 +0300

    feat: adding more attributes to trip computer

--- src/main/java/org/example/components/TripComputer.java
+++ src/main/java/org/example/components/TripComputer.java
@@ -1,4 +1,26 @@
 package org.example.components;
 
 public class TripComputer {
+    private int currentSpeed = 0;
+    private double fuelLevel;
+
+    public TripComputer(double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void setFuelLevel(double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
 }
