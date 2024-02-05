3b6f806c303fe34233ff76c719632a304f2a46f4
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-02-05 16:23:58 +0300

    feat: car manual engine

--- src/main/java/org/example/product/Car.java
+++ src/main/java/org/example/product/Car.java
@@ -8,6 +8,17 @@ import org.example.components.TripComputer;
 public class Car {
     private Integer seats;
     private Type type;
+    private Engine engine;
+    private TripComputer tripComputer;
+    private GPS gps;
+
+    public Car(Integer seats, Engine engine, TripComputer tripComputer, GPS gps, Type type) {
+        this.seats = seats;
+        this.engine = engine;
+        this.tripComputer = tripComputer;
+        this.gps = gps;
+        this.type = type;
+    }
 
     public Integer getSeats() {
         return seats;
@@ -39,18 +50,6 @@ public class Car {
 
     public void setGps(GPS gps) {
         this.gps = gps;
-    }
-
-    private Engine engine;
-    private TripComputer tripComputer;
-    private GPS gps;
-
-    public Car(Integer seats, Engine engine, TripComputer tripComputer, GPS gps, Type type) {
-        this.seats = seats;
-        this.engine = engine;
-        this.tripComputer = tripComputer;
-        this.gps = gps;
-        this.type = type;
     }
 
     public Type getType() {
